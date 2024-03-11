2b4a4a767a41b49e003fa3a5a84f581942defa9c
Jocelyn Tang <jocelynt8ng@gmail.com>
2024-03-11 18:14:03 +0800

    add shark sprites

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pygame
 from fish import Fish
 from player import Player
+from shark import Shark
 
 # pygame setup
 pygame.init()
@@ -19,13 +20,20 @@ player = Player(screen)
 fish_list = pygame.sprite.Group()
 fish_generation_timer = 0
 
+shark_list = pygame.sprite.Group()
+
 # Initialize fish sprites
 for _ in range(4):
     fish = Fish(screen)
     fish_list.add(fish)
 
+# Initialize shark sprites
+for _ in range(3):
+    shark = Shark(screen)
+    shark_list.add(shark)
+
 while running:
-    if fish_generation_timer > 300:
+    if fish_generation_timer > 200:
         fish_list.add(Fish(screen))
         fish_generation_timer = 0
 
@@ -43,9 +51,15 @@ while running:
     player.update(dt, keys)
     player.draw()
 
-    # Update and draw all fish
+    # Update and draw all fishes
     fish_list.update()
     fish_list.draw(screen)
+
+    # Update and draw all sharks
+    for shark in shark_list:
+        shark.update()
+        shark.animate()
+        shark.draw()
 
     # Check collision with fish
     for fish in fish_list:
---
+++ shark.py
@@ -0,0 +1,57 @@
+import random
+import pygame
+
+class Shark(pygame.sprite.Sprite):
+    
+    def __init__(self, screen):
+        super().__init__()
+        self.screen = screen
+        self.NUM_FRAMES = 2
+        self.shark_frames = [pygame.image.load(f"assets/frames/SharkCharacter_{i}.png") for i in range(self.NUM_FRAMES)]
+        self.shark_rect = self.shark_frames[0].get_rect()
+        self.shark_rect.x = random.randint(0, screen.get_width() - self.shark_rect.width)
+        self.shark_rect.y = random.randint(0, screen.get_height() - self.shark_rect.height)
+        self.speedx = random.randint(-5, 5)
+        self.speedy = random.randint(-5, 5)
+
+        # Load player animation frames
+        self.current_frame = 0
+        self.frame_counter = 0
+        self.ANIMATION_SPEED = 15
+        # Set the initial player image
+        self.shark_image = self.shark_frames[self.current_frame]
+
+        # Initial player direction
+        self.shark_direction = 'right'
+
+    def update(self):
+        # Update position based on speed
+        self.shark_rect.x += self.speedx
+        self.shark_rect.y += self.speedy
+
+        # Reverse direction if hitting the screen edge
+        if self.shark_rect.left < 0 or self.shark_rect.right > self.screen.get_width():
+            self.speedx *= -1
+            if self.speedx > 0:
+                self.shark_direction = 'left'
+            else:
+                self.shark_direction = 'right'
+
+        if self.shark_rect.top < 0 or self.shark_rect.bottom > self.screen.get_height():
+            self.speedy *= -1
+
+    def animate(self):
+        # Animate player
+        self.frame_counter += 1
+        if self.frame_counter >= self.ANIMATION_SPEED:
+            self.frame_counter = 0
+            self.current_frame = (self.current_frame + 1) % self.NUM_FRAMES
+
+    def draw(self):
+        # Draw sharks
+        self.shark_image = self.shark_frames[self.current_frame]
+        if self.shark_direction == 'right':
+            self.screen.blit(self.shark_image, self.shark_rect)
+        else:  # Flip the image horizontally if the player is facing left
+            flipped_player_image = pygame.transform.flip(self.shark_image, True, False)
+            self.screen.blit(flipped_player_image, self.shark_rect)
